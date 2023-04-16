10555d97916b8404ba1bd11c203e42189feffe00
William Wang <wwang2@atlassian.com>
2023-04-17 07:19:30 +1000

    v 0.2.0

--- src/simple_flask_restful/__init__.py
+++ src/simple_flask_restful/__init__.py
@@ -1,4 +1,15 @@
+"""
+simple_flask_restful
+
+
+This library is a lightweight Python package that provides a simpler interface to create RESTful APIs using Flask.
+"""
+
 from .simple_restful import Api, Resource
 from .restful_exceptions import abort
 from . import reqparse
 from flask import Flask
+
+
+__version__ = '0.2.0'
+__all__ = ['Api', 'Resource', 'abort', 'reqparse', 'Flask']
